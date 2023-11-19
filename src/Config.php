29733e5172f90e82dabbdae1248999e31456b336
Eduardo C. Resplandes <109596877+EResplandes@users.noreply.github.com>
2023-11-19 09:15:06 -0300

    Update Config.php
    Segurança

--- src/Config.php
+++ src/Config.php
@@ -5,7 +5,7 @@ class Config {
     const BASE_DIR = '/mvc/public';
 
     const DB_DRIVER = 'mysql';
-    const DB_HOST = '10.166.65.94';
+    const DB_HOST = 'localhost';
     const DB_DATABASE = 'db_direcao_dev';
     CONST DB_USER = 'webserver';
     const DB_PASS = 'database';
